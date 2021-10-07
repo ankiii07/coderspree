4fc25acffc7b3f91699ad8568f26cde36b5d0f83
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-10-08 00:08:31 +0530

    digits

---
+++ ML/ankiii07_AnkitYadav_1923co1041_3/GettingStarted/Digits Of A Number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+    
+    public class Main{
+      	
+	static void printDigit(int N)
+	{
+	    int r;
+	    if (N == 0) 
+	    {
+	        return;
+	    }
+	    r = N % 10;
+	    
+	    printDigit(N / 10);
+	  
+	    System.out.println(r);
+	}
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		printDigit(N);
+	}
+}
